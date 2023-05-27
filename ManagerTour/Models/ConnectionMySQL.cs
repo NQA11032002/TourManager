213ba0ea1e4b57660c7863fe53f2583e02023a0c
NQA11032002 <kawasakisga@gmail.com>
2023-05-28 01:14:04 +0700

    update code connection mysql

--- ManagerTour/Models/ConnectionMySQL.cs
+++ ManagerTour/Models/ConnectionMySQL.cs
@@ -11,34 +11,42 @@ namespace ManagerTour.Models
     {
         string strConnect = "Server=localhost;Port=3306;Database=tour;Uid=root;Pwd=;";
 
-        public MySqlConnection ConnectionSql()
+        //get value from database
+        public DataSet SelectData(string sql)
         {
-            MySqlConnection Connection = new MySqlConnection(strConnect);
-
-            try
+            using(MySqlConnection Connection = new MySqlConnection(strConnect))
             {
                 Connection.Open();
 
-                return Connection;
-            }
-            finally
-            {
-                Connection.Close();
+                MySqlCommand command = new MySqlCommand(sql, Connection);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                return ds;
             }
         }
 
-        public DataSet SelectData(string sql)
+        //insert value to database
+        public bool InsertData(MySqlCommand cmd)
         {
-            MySqlConnection Connection = new MySqlConnection(strConnect);
-            Connection = ConnectionSql();
 
-            MySqlCommand command = new MySqlCommand(sql, Connection);
-            command.Connection = Connection;
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-            adapter.SelectCommand = command;
-            DataSet ds = new DataSet();
-            adapter.Fill(ds);
-            return ds;
+            using (MySqlConnection Connection = new MySqlConnection(strConnect))
+            {
+                Connection.Open();
+
+                int result = cmd.ExecuteNonQuery();
+
+                if (result >= 1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
     }
 }
