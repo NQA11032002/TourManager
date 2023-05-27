a775b6bba6bb7dfb4fc01cb5398b98a195cde88a
NQA11032002 <kawasakisga@gmail.com>
2023-05-27 23:47:12 +0700

    update first code function template

---
+++ ManagerTour/Content/js/home.js
@@ -0,0 +1,42 @@
+﻿const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const pie = document.getElementById('myChart_1');
+
+const data = {
+    datasets: [{
+        data: [10, 20]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Tour',
+        'Social',
+    ]
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+};
+
+
+new Chart(pie, config);
---
+++ ManagerTour/Models/ConnectionMySQL.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Tour.Models
+{
+    public class ConnectionMySQL
+    {
+        string strConnect = "Server=localhost;Port=3306;Database=tour;Uid=root;Pwd=;";
+
+        public MySqlConnection ConnectionSql()
+        {
+            MySqlConnection Connection = new MySqlConnection(strConnect);
+
+            try
+            {
+                Connection.Open();
+
+                return Connection;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+
+        public DataSet SelectData(string sql)
+        {
+            MySqlConnection Connection = new MySqlConnection(strConnect);
+            Connection = ConnectionSql();
+
+            MySqlCommand command = new MySqlCommand(sql, Connection);
+            command.Connection = Connection;
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            return ds;
+        }
+    }
+}
